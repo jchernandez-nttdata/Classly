064670b8ed28a1ed979507cc7973b83f056cfb9d
Juan Carlos Hernandez Castillo <juancarlos.hernandezcastillo+pacifico@emeal.nttdata.com>
2025-05-06 22:10:39 -0500

    Add StudentManagement package

---
+++ StudentManagement/Package.swift
@@ -0,0 +1,38 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "StudentManagement",
+    platforms: [
+        .iOS(.v16)
+    ],
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "StudentManagement",
+            targets: ["StudentManagement"]),
+    ],
+    dependencies: [
+        .package(path: "../Assets"),
+        .package(path: "../Core"),
+        .package(path: "../UIComponents")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "StudentManagement",
+            dependencies: [
+                "Assets",
+                "Core",
+                "UIComponents"
+            ]
+        ),
+        .testTarget(
+            name: "StudentManagementTests",
+            dependencies: ["StudentManagement"]
+        ),
+    ]
+)
