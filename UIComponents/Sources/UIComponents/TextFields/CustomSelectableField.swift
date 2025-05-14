d39ac1fb512596bbed8bd9e587ea9d8e900d1acb
Juan Carlos Hernandez Castillo <jherncas@emeal.nttdata.com>
2025-05-14 15:38:49 -0500

    Add CustomSelectableField

---
+++ UIComponents/Sources/UIComponents/TextFields/CustomSelectableField.swift
@@ -0,0 +1,92 @@
+//
+//  CustomSelectableField.swift
+//  UIComponents
+//
+//  Created by Juan Carlos Hernandez Castillo on 14/05/25.
+//
+
+import SwiftUI
+import Assets
+
+public struct CustomSelectableField: View {
+    @Binding private var selectedItem: String?
+    private let items: [String]
+    private let placeholder: String
+
+    @State private var showSheet = false
+
+    public init(
+        selectedItem: Binding<String?>,
+        items: [String],
+        placeholder: String = "Select an option"
+    ) {
+        self._selectedItem = selectedItem
+        self.items = items
+        self.placeholder = placeholder
+    }
+
+    public var body: some View {
+        Button(action: {
+            showSheet.toggle()
+        }) {
+            HStack {
+                Text(selectedItem ?? placeholder)
+                    .foregroundColor(selectedItem == nil ? AppColor.disabledText : AppColor.primaryText)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+
+                Spacer()
+
+                Image(systemName: "chevron.down")
+                    .foregroundColor(AppColor.primaryText)
+            }
+            .padding(.vertical, 13)
+            .padding(.horizontal, 15)
+            .background(AppColor.background)
+            .cornerRadius(16)
+            .overlay(
+                RoundedRectangle(cornerRadius: 16)
+                    .stroke(AppColor.border, lineWidth: 1)
+            )
+        }
+        .sheet(isPresented: $showSheet) {
+            VStack(spacing: 0) {
+                HStack {
+                    Spacer()
+                    Button("Done") {
+                        showSheet = false
+                    }
+                    .padding(.all, 15)
+                }
+
+                Divider()
+
+                Spacer()
+
+                Picker("Select an option", selection: $selectedItem) {
+                    ForEach(items, id: \.self) { item in
+                        Text(item).tag(item)
+                    }
+                }
+                .pickerStyle(.wheel)
+                .labelsHidden()
+                .frame(height: 250)
+
+                Spacer()
+            }
+            .presentationDetents([.height(310)])
+            .padding(.all, 0)
+        }
+
+    }
+}
+
+
+#Preview {
+    VStack {
+        CustomSelectableField(
+            selectedItem: .constant(nil),
+            items: (1...20).map { "Option \($0)" }
+        )
+        .padding()
+    }
+}
